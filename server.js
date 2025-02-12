805d733706b7ef1db545f7cdc4fad2a0b609ea67
ManDuong <manshpypro@gmail.com>
2025-02-12 01:08:26 -0800

    video 13

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n Duong Cong Man');
+    res.end('Hello World\n Duong Cong Man ne ae');
 });
 
 server.listen(port, hostname, () => {
